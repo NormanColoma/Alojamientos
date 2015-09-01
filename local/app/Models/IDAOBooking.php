ce4b854473d3cd7552d6cac2991528914c1465dd
Javi <jaNviier@gmail.com>
2015-09-01 19:54:58 +0200

    BookingModel, Booking, PreBooking e IDAOBooking implementados. También test de integración y de unidad

---
+++ local/app/Models/IDAOBooking.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: Javier
+ * Date: 01/09/2015
+ * Time: 18:13
+ */
+
+namespace App\Models;
+
+use App\Models\DTO\Booking;
+
+interface IDAOBooking
+{
+
+    public function createBooking(Booking $booking, $userId, $accommId);
+    public function updateBooking(Booking $booking, $id);
+    public function deleteBooking($id);
+    public function showBooking();
+    public function showPreBooking();
+    public function confirm($id);
+
+}
